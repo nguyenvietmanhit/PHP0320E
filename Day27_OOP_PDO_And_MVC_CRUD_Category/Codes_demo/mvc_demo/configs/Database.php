9d47a5ccd6f81b58d4434014294463bf9a9aa0d5
Your Name <you@example.com>
2020-08-03 21:30:31 +0700

    a

--- Day27_OOP_PDO_And_MVC_CRUD_Category/Codes_demo/mvc_demo/configs/Database.php
+++ Day27_OOP_PDO_And_MVC_CRUD_Category/Codes_demo/mvc_demo/configs/Database.php
@@ -3,7 +3,7 @@
 //Class chứa các hằng số kết nối CSDL
 //Tên class sẽ trùng với tên file
 class Database {
-  const DB_DSN = '';
-  const DB_USERNAME = '';
+  const DB_DSN = 'mysql:host=localhost;dbname=php0320e_mvc;port=3306;charset=utf8';
+  const DB_USERNAME = 'root';
   const DB_PASSWORD = '';
 }
--- Day27_OOP_PDO_And_MVC_CRUD_Category/Codes_demo/mvc_demo/views/layouts/main.php
+++ Day27_OOP_PDO_And_MVC_CRUD_Category/Codes_demo/mvc_demo/views/layouts/main.php
@@ -13,7 +13,15 @@
   <?php require_once 'header.php'; ?>
 </div>
 <div class="main-content">
+<!--  Hiển thị tất cả các thông báo liên quan đến lỗi,
+  session lỗi , thành công tại file layout -->
+    <h3 style="color: red">
+        <?php echo $this->error; ?>
+    </h3>
+
     <!--  Hiển thi nội dung động  -->
+<!-- cơ chế render view động   -->
+    <?php echo $this->content; ?>
 </div>
 <!--  Nhúng footer  -->
 <div class="footer">
